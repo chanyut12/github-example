d2ca7bf55d23fc19db338bb323ef217c6df590f8
Overdose <80890008+chanyut12@users.noreply.github.com>
2023-08-07 21:18:26 +0700

    Add files via upload

---
+++ lab_6/input4.py
@@ -0,0 +1,7 @@
+a=int(input())
+for i in range(1,a+1):
+    if i%5==0:
+        print("X",end="")
+    else:
+        print("*",end="")    
+print()
---
+++ lab_6/input6.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=0
+N=a
+for i in range(a):
+    for j in range(N):
+        print(i+1, end=" ")
+    print()
+    N-=1    
+    
