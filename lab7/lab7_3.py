151c23c16e2d1131843e68cc4caa5bdadd0b007a
Overdose <80890008+chanyut12@users.noreply.github.com>
2023-08-21 23:22:15 +0700

    lab7_3.py

--- lab7/lab7_3.py
+++ lab7/lab7_3.py
@@ -1,5 +1,5 @@
 a=input()
 print(a.swapcase())
-print(a.capitalize())
+print(a.capitalize()) # converts the first character of a string to an uppercase letter and all other alphabets to lowercase.
 print(a.upper())
 print(a.lower())
