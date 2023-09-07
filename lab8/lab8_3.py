7261cc0358f49a99f688bef7b02d871ec33e2c62
Overdose <80890008+chanyut12@users.noreply.github.com>
2023-09-07 14:49:51 +0700

    lab8_3.py

--- lab8/lab8_3.py
+++ lab8/lab8_3.py
@@ -17,37 +17,38 @@ def main():
         N = int(input())
         
         # Initialize an empty list to store N integers
-        num_list = []
+        list = []
 
         # Receive N integers and store them in the list
         for i in range(N):
             num = int(input())
-            num_list.append(num)
+            list.append(num)
 
         # Receive the integer to search for
         search_value = int(input())
 
         # Search for the value in the list
-        index = find_index(num_list, search_value)
+        index = find_index(list, search_value)
 
         # Display the result
         if index != -1:
-            print(find_index(num_list, search_value))
+            print(find_index(list, search_value))
         else:
             print(-1)
 
-def find_index(num_list, search_value):
+def find_index(list, search_value):
     """
     Function to find the index of the search_value in the list.
     Returns -1 if the value is not found.
     """
-    for i, num in enumerate(num_list): #enumerate ดึง index ของค่านั้น ๆ
+    for i, num in enumerate(list): #enumerate ดึง index ของค่านั้น ๆ
         if num == search_value:
             return i
     return -1
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+   # main()
 
+main()
         
 
